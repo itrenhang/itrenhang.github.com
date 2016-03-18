9a1bdcffe8880e4eb05688a869e94e0c07e72ae0
itrenhang <itrenhang@sina.com>
2016-03-18 23:53:56 +0800

    this

--- js/MoveH.js
+++ js/MoveH.js
@@ -1,1 +1,1 @@
-"use strict";function MoveH(a,b){var c=0,d=0,e=null;e=setInterval(function(){c+=3,d=a.offsetHeight+c,d>b&&(d=b,c*=-.5),a.style.height=d+"px",Math.abs(c)<1&&(c=0),0==c&&d==b&&clearInterval(e)},30)}
+"use strict";function MoveH(a,b){var c=0,d=0,e=null,f=0;clearInterval(e),e=setInterval(function(){c+=3,d=a.offsetHeight+c,d>b&&(d=b,c*=-.5),f++,document.title=f,a.style.height=d+"px",Math.abs(c)<1&&(c=0),0==c&&d==b&&clearInterval(e)},30)}
